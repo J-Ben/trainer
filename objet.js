cb48be6213d4aa5c2c8b678ceedc6683bc9a13aa
Ben-J MK <ben-jamin@anjcprod.org>
2024-06-08 13:04:32 +0200

    feat(lesson): chap 2

---
+++ objet.js
@@ -0,0 +1,28 @@
+Fruits = [
+     "1" : {
+        label="Ananas",
+        color: "yellow",
+        taste: "sweet",
+        size: "medium",
+        price: 1000,
+      }, 
+       "2" : {        
+        label="Apple",
+        color: "red",
+        taste: "sour",
+        size: "small",
+        price: 500,
+        variety: "Fuji",
+      }, 
+      "3" : {
+        label="Banana",
+        color: "yellow",
+        taste: "sour",
+        size: "medium",
+        price: 1000,
+        variety: "Cavendish",
+        cooking: {
+          method: "boiling",
+          time: 10,
+        } 
+    ];
